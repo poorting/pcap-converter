403f04153097f35b8fbc1f06cdc5b627d773ddc2
poorting <remco.poortinga@surf.nl>
2024-03-02 23:43:15 +0100

    Switch to Apache parquet writer

---
+++ src/packetstats.rs
@@ -0,0 +1,246 @@
+use std::fmt::Debug;
+use anyhow::Error;
+use arrow2::offset;
+use pcap_parser::data::PacketData;
+use etherparse::*;
+use etherparse::icmpv4::TYPE_DEST_UNREACH;
+use pcap_parser::nom::Err;
+use std::net::*;
+use domain::base::*;
+
+
+#[derive(Default, Debug, Clone)]
+pub struct PacketStats {
+    pub frame_time: Option<i64>,
+    pub frame_len: Option<u32>,
+    pub eth_type: Option<u16>,
+    pub ip_id: Option<u16>,
+    pub ip_src: Option<String>,
+    pub ip_dst: Option<String>,
+    pub ip_proto: Option<u8>,
+    pub ip_ttl: Option<u8>,
+    pub ip_frag_offset: Option<u16>,
+    pub icmp_type: Option<u8>,
+    pub udp_length: Option<u16>,
+    pub udp_srcport: Option<u16>,
+    pub udp_dstport: Option<u16>,
+    pub tcp_flags: Option<String>,
+    pub tcp_srcport: Option<u16>,
+    pub tcp_dstport: Option<u16>,
+    pub col_info: Option<String>,
+    pub col_source: Option<String>,
+    pub col_destination: Option<String>,
+    pub col_protocol: Option<String>,
+    pub dns_qry_name: Option<String>,
+    pub dns_qry_type: Option<u16>,
+    pub http_request_uri: Option<String>,
+    pub http_host: Option<String>,
+    pub http_request_method: Option<String>,
+    pub http_user_agent: Option<String>,
+    pub http_file_data: Option<String>,
+    pub ntp_priv_reqcpde: Option<u8>,
+    pub ip_total_len: u16,
+    more_fragments: bool,
+}
+
+impl PacketStats {
+    pub fn new() -> PacketStats {
+        // return Default::default()
+        PacketStats { ..Default::default()}
+    }
+
+    pub fn copy_from_no_frame(&mut self, other: &PacketStats) {
+
+        let frame_len = self.frame_len;
+        let frame_time = self.frame_time;
+
+        // let mut this = other.clone();
+        *self = other.clone();
+
+        self.frame_len = frame_len;
+        self.frame_time = frame_time;
+    }
+
+    pub fn is_fragment(&mut self) -> bool {
+        match self.ip_frag_offset {
+            Some(offset) => offset > 0,
+            None => false,
+        }
+    }
+
+    pub fn is_first_fragment(&mut self) -> bool {
+        self.more_fragments && !self.is_fragment()
+    }
+
+    fn tcp_flags_as_string(&mut self, tcp: TcpHeader) -> String {
+        let mut flags = String::from("........");
+        if tcp.fin {
+            flags.replace_range(7..8, "F")
+        };
+        if tcp.syn {
+            flags.replace_range(6..7, "S")
+        };
+        if tcp.rst {
+            flags.replace_range(5..6, "R")
+        };
+        if tcp.psh {
+            flags.replace_range(4..5, "P")
+        };
+        if tcp.ack {
+            flags.replace_range(3..4, "A")
+        };
+        if tcp.urg {
+            flags.replace_range(1..3, "U")
+        };
+        if tcp.ece {
+            flags.replace_range(1..2, "E")
+        };
+        if tcp.cwr {
+            flags.replace_range(0..1, "C")
+        };
+
+        return flags;
+    }
+
+
+    pub fn analyze_packet(&mut self, pkt_data: PacketData) -> Result<(),Error> {
+        match pkt_data {
+            PacketData::L2(eth_data) => {
+                self.analyze_packet_headers(PacketHeaders::from_ethernet_slice(eth_data)?);
+            }
+            PacketData::L3(_, ip_data) => {
+                self.analyze_packet_headers(PacketHeaders::from_ip_slice(ip_data)?);
+            }
+            _ => todo!(),
+        };
+
+        Ok(())
+    }
+
+
+    fn analyze_packet_headers(&mut self, pkt_headers: PacketHeaders) {
+        let EtherType(et) = pkt_headers.link.unwrap().ether_type;
+        self.eth_type = Some(et);
+        match pkt_headers.net {
+            Some(NetHeaders::Ipv4(ip, _)) => {
+                // May be replaced by transport or application protocol later on
+                self.col_protocol = Some("IPv4".to_string());
+                // self.frame_len = Some(ip.total_len as u32);
+                self.ip_id = Some(ip.identification);
+                self.ip_total_len = ip.total_len;
+
+                self.ip_src = Some(Ipv4Addr::from(ip.source).to_string());
+                self.ip_dst = Some(Ipv4Addr::from(ip.destination).to_string());
+                self.col_source = Some(Ipv4Addr::from(ip.source).to_string());
+                self.col_destination =
+                    Some(Ipv4Addr::from(ip.destination).to_string());
+                self.ip_ttl = Some(ip.time_to_live);
+                self.ip_proto = Some(u8::from(ip.protocol));
+
+                let frag_offset = u16::from(ip.fragment_offset);
+                self.more_fragments = ip.more_fragments;
+                self.ip_frag_offset = Some(frag_offset);
+            }
+            Some(NetHeaders::Ipv6(ip, _)) => {
+                // May be replaced by transport or application protocol later on
+                self.col_protocol = Some("IPv6".to_string());
+                // self.frame_len = Some((ip.payload_length+ip.header_len() as u16) as u32);
+                self.ip_src = Some(Ipv6Addr::from(ip.source).to_string());
+                self.ip_dst = Some(Ipv6Addr::from(ip.destination).to_string());
+                self.col_source = Some(Ipv6Addr::from(ip.source).to_string());
+                self.col_destination =
+                    Some(Ipv6Addr::from(ip.destination).to_string());
+                self.ip_ttl = Some(ip.hop_limit);
+                self.ip_proto = Some(u8::from(ip.next_header));
+            }
+            _ => (),
+        }
+
+        match pkt_headers.transport {
+            Some(TransportHeader::Udp(udp)) => {
+
+                // May be replaced by transport protocol later on
+                self.col_protocol = Some("UDP".to_string());
+
+                self.udp_srcport = Some(udp.source_port);
+                self.udp_dstport = Some(udp.destination_port);
+                self.udp_length = Some(udp.length);
+
+                if udp.source_port == 53 || udp.destination_port == 53 {
+                    self.col_protocol = Some("DNS".to_string());
+                    match Message::from_octets(&pkt_headers.payload.slice()) {
+                        Ok(dns) => {
+                            match dns.first_question() {
+                                Some(question) => {
+                                    let name = if question.qname().is_root() {
+                                        "<Root>".to_string()
+                                    } else {
+                                        question.qname().to_string()
+                                    };
+                                    self.dns_qry_name = Some(name.clone());
+                                    self.dns_qry_type = Some(question.qtype().to_int());
+                                }
+                                _ => ()
+                            }
+                        }
+                        Err(_e) => {
+                            eprintln!("{}", _e);
+                        }
+                    }
+                }
+            }
+
+            Some(TransportHeader::Tcp(tcp)) => {
+                // May be replaced by transport protocol later on
+                self.col_protocol = Some("TCP".to_string());
+
+                self.tcp_srcport = Some(tcp.source_port);
+                self.tcp_dstport = Some(tcp.destination_port);
+                let flags = self.tcp_flags_as_string(tcp);
+                self.tcp_flags = Some(flags);
+            }
+            Some(TransportHeader::Icmpv4(icmp)) => {
+                // May be replaced by transport or application protocol later on
+                self.col_protocol = Some("ICMP".to_string());
+
+                let bytes = icmp.to_bytes();
+                self.icmp_type = Some(bytes[0]);
+                if bytes[0] == TYPE_DEST_UNREACH {
+                    // Payload contains header of the original packet
+                    // eprintln!("{:?}", pkt_headers.payload);
+                    match PacketHeaders::from_ip_slice(pkt_headers.payload.slice()) {
+                        Ok(icmp_ph) => {
+                            // eprintln!("{:#?}", icmp_ph);
+                            match icmp_ph.transport {
+                                Some(TransportHeader::Udp(udp)) => {
+                                    // eprintln!("UDP: {:?}", udp);
+                                    self.udp_srcport = Some(udp.source_port);
+                                    self.udp_dstport = Some(udp.destination_port);
+                                }
+                                Some(TransportHeader::Tcp(tcp)) => {
+                                    // eprintln!("TCP: {:#?}", tcp);
+                                    self.tcp_srcport = Some(tcp.source_port);
+                                    self.tcp_dstport = Some(tcp.destination_port);
+                                    let mut flags = self.tcp_flags_as_string(tcp);
+                                    self.tcp_flags = Some(flags);
+                                }
+                                _ => (),
+                            }
+                        }
+                        Err(_) => (),
+                    }
+                }
+            }
+            Some(TransportHeader::Icmpv6(icmp)) => {
+                // eprintln!("ICMPv6: {:#?}", icmp)
+                // self.packet_detail.icmp_type = Some(u8::from(icmp.icmp_type));
+            }
+            _ => (),
+        }
+    }
+
+    pub fn set_ip_src(&mut self, ip_src: String) { self.ip_src = Some(ip_src)}
+    pub fn set_frame_time(&mut self, frame_time: i64) { self.frame_time = Some(frame_time)}
+
+}
+
---
+++ src/statswriter.rs
@@ -0,0 +1,235 @@
+use std::{fmt::Debug, sync::Arc};
+use std::path::Path;
+use std::collections::HashMap;
+use std::fs::File;
+use crate::packetstats::*;
+use anyhow::Error;
+use num_format::{Locale, ToFormattedString};
+use arrow::datatypes::*;
+use arrow::array::*;
+use arrow::datatypes::DataType::*;
+use parquet::{
+    basic::{Compression, Encoding},
+    file::properties::*,
+    schema::types::ColumnPath,
+    arrow::ArrowWriter,
+};
+
+
+
+
+#[derive(Debug)]
+pub struct StatsWriter {
+    // filename: String,
+    // fields: Vec<Field>,
+    pcap_file: String,
+    schema: Schema,
+    pub writer: ArrowWriter<std::fs::File>,
+    packets: Vec<PacketStats>,
+    frag_cache: HashMap<u16, PacketStats>,
+    packet_count: i64,
+    cache_misses: i64,
+    verbose: bool,
+ }
+
+impl StatsWriter {
+    pub fn new(filename: &str, pcap_file: &str, verbose: bool) -> Result<StatsWriter, Error> {
+        // return Default::default()
+
+        let fields = StatsWriter::create_fields();
+        let schema = Schema::new(fields.clone());
+        let props = WriterProperties::builder()
+        .set_writer_version(WriterVersion::PARQUET_1_0)
+        .set_encoding(Encoding::PLAIN)
+        .set_column_encoding(ColumnPath::from("col1"), Encoding::DELTA_BINARY_PACKED)
+        .set_compression(Compression::SNAPPY)
+        .build();
+    
+        let file = File::create(filename).unwrap();
+
+        let writer = ArrowWriter::try_new(file, Arc::new(schema.clone()), Some(props))?;
+    
+        let path = Path::new(pcap_file);
+        let filename = path.file_name().unwrap();
+        let sw = StatsWriter { 
+            // filename: filename.to_string(),
+            // fields: StatsWriter::create_fields(), 
+                pcap_file: filename.to_str().unwrap().to_string(),
+                schema: schema,
+                writer: writer,
+                packets: Vec::new(),
+                frag_cache: HashMap::new(),
+                packet_count: 0,
+                cache_misses: 0,
+                verbose: verbose,
+            };
+    
+        Ok(sw)
+    }
+
+    fn create_fields() -> Vec<Field> {
+        let mut fields: Vec<Field> = Vec::new();
+    
+        fields.push(Field::new(
+            "frame_time",
+            Timestamp(TimeUnit::Microsecond, None),
+            true,
+        ));
+        fields.push(Field::new("frame_len", UInt32, true));
+        fields.push(Field::new("eth_type", UInt16, true));
+        fields.push(Field::new("ip_src", Utf8, true));
+        fields.push(Field::new("ip_dst", Utf8, true));
+        fields.push(Field::new("ip_proto", UInt8, true));
+        fields.push(Field::new("ip_ttl", UInt8, true));
+        fields.push(Field::new("ip_frag_offset", UInt16, true));
+        fields.push(Field::new("icmp_type", UInt8, true));
+        fields.push(Field::new("udp_length", UInt16, true));
+        fields.push(Field::new("udp_srcport", UInt16, true));
+        fields.push(Field::new("udp_dstport", UInt16, true));
+        fields.push(Field::new("tcp_flags", Utf8, true));
+        fields.push(Field::new("tcp_srcport", UInt16, true));
+        fields.push(Field::new("tcp_dstport", UInt16, true));
+        fields.push(Field::new("col_info", Utf8, true));
+        fields.push(Field::new("col_source", Utf8, true));
+        fields.push(Field::new("col_destination", Utf8, true));
+        fields.push(Field::new("col_protocol", Utf8, true));
+        fields.push(Field::new("dns_qry_name", Utf8, true));
+        fields.push(Field::new("dns_qry_type", UInt16, true));
+        fields.push(Field::new("http_request_uri", Utf8, true));
+        fields.push(Field::new("http_host", Utf8, true));
+        fields.push(Field::new("http_request_method", Utf8, true));
+        fields.push(Field::new("http_user_agent", Utf8, true));
+        fields.push(Field::new("http_file_data", Utf8, true));
+        fields.push(Field::new("ntp_priv_reqcode", UInt8, true));
+        fields.push(Field::new("pcap_file", Utf8, true));
+    
+        fields
+    }
+    
+    pub fn push(&mut self, mut packet: PacketStats) {
+
+        // See if this is a first fragment
+        let ip_id = packet.ip_id.unwrap_or_default();
+
+        if packet.is_first_fragment() {
+            self.frag_cache.entry(ip_id).or_insert(packet.clone());
+        } else if packet.is_fragment() {
+            // See if we have a match for this fragment
+            match self.frag_cache.get(&ip_id) {
+                Some(cache) => {
+                    packet.udp_srcport = cache.udp_srcport;
+                    packet.udp_dstport = cache.udp_dstport;
+                    packet.udp_length = Some(cache.ip_total_len);
+                    packet.dns_qry_type = cache.dns_qry_type;
+                    packet.dns_qry_name = cache.dns_qry_name.clone();
+                }
+                None => {
+                    // cache miss
+                    self.cache_misses += 1;
+                }
+            }
+        }
+
+        self.packets.push(packet);
+        self.packet_count += 1;
+
+        if self.packets.len() >= 50_000 {
+            self.write_batch();
+        }
+
+    }
+
+    fn record_batch(&mut self) -> RecordBatch {
+        let frame_time = TimestampMicrosecondArray::from(self.packets.iter().map(|p| p.frame_time).collect::<Vec<Option<i64>>>());
+        let frame_len = UInt32Array::from(self.packets.iter().map(|p| p.frame_len).collect::<Vec<Option<u32>>>());
+        let eth_type = UInt16Array::from(self.packets.iter().map(|p| p.eth_type).collect::<Vec<Option<u16>>>());
+        let ip_src = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.ip_src.clone()).collect::<Vec<Option<String>>>());
+        let ip_dst = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.ip_dst.clone()).collect::<Vec<Option<String>>>());
+        let ip_proto = UInt8Array::from(self.packets.iter().map(|p| p.ip_proto).collect::<Vec<Option<u8>>>());
+        let ip_ttl = UInt8Array::from(self.packets.iter().map(|p| p.ip_ttl).collect::<Vec<Option<u8>>>());
+        let ip_frag_offset = UInt16Array::from(self.packets.iter().map(|p| p.ip_frag_offset).collect::<Vec<Option<u16>>>());
+        let icmp_type = UInt8Array::from(self.packets.iter().map(|p| p.icmp_type).collect::<Vec<Option<u8>>>());
+        let udp_length = UInt16Array::from(self.packets.iter().map(|p| p.udp_length).collect::<Vec<Option<u16>>>());
+        let udp_srcport = UInt16Array::from(self.packets.iter().map(|p| p.udp_srcport).collect::<Vec<Option<u16>>>());
+        let udp_dstport = UInt16Array::from(self.packets.iter().map(|p| p.udp_dstport).collect::<Vec<Option<u16>>>());
+        let tcp_flags = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.tcp_flags.clone()).collect::<Vec<Option<String>>>());
+        let tcp_srcport = UInt16Array::from(self.packets.iter().map(|p| p.tcp_srcport).collect::<Vec<Option<u16>>>());
+        let tcp_dstport = UInt16Array::from(self.packets.iter().map(|p| p.tcp_dstport).collect::<Vec<Option<u16>>>());
+        let col_info = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.col_info.clone()).collect::<Vec<Option<String>>>());
+        let col_source = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.col_source.clone()).collect::<Vec<Option<String>>>());
+        let col_destination = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.col_destination.clone()).collect::<Vec<Option<String>>>());
+        let col_protocol = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.col_protocol.clone()).collect::<Vec<Option<String>>>());
+        let dns_qry_name = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.dns_qry_name.clone()).collect::<Vec<Option<String>>>());
+        let dns_qry_type = UInt16Array::from(self.packets.iter().map(|p| p.dns_qry_type).collect::<Vec<Option<u16>>>());
+        let http_request_uri = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.http_request_uri.clone()).collect::<Vec<Option<String>>>());
+        let http_host = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.http_host.clone()).collect::<Vec<Option<String>>>());
+        let http_request_method = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.http_request_method.clone()).collect::<Vec<Option<String>>>());
+        let http_user_agent = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.http_user_agent.clone()).collect::<Vec<Option<String>>>());
+        let http_file_data = GenericStringArray::<i32>::from(self.packets.iter().map(|p| p.http_file_data.clone()).collect::<Vec<Option<String>>>());
+        let nttp_priv_reqcode = UInt8Array::from(self.packets.iter().map(|p| p.ntp_priv_reqcpde).collect::<Vec<Option<u8>>>());
+        let pcap_file = GenericStringArray::<i32>::from(vec![self.pcap_file.clone(); self.packets.len()]);
+        
+
+        let batch = RecordBatch::try_new(
+            Arc::new(self.schema.clone()),
+            vec![
+                Arc::new(frame_time),
+                Arc::new(frame_len),
+                Arc::new(eth_type),
+                Arc::new(ip_src),
+                Arc::new(ip_dst),
+                Arc::new(ip_proto),
+                Arc::new(ip_ttl),
+                Arc::new(ip_frag_offset),
+                Arc::new(icmp_type),
+                Arc::new(udp_length),
+                Arc::new(udp_srcport),
+                Arc::new(udp_dstport),
+                Arc::new(tcp_flags),
+                Arc::new(tcp_srcport),
+                Arc::new(tcp_dstport),
+                Arc::new(col_info),
+                Arc::new(col_source),
+                Arc::new(col_destination),
+                Arc::new(col_protocol),
+                Arc::new(dns_qry_name),
+                Arc::new(dns_qry_type),
+                Arc::new(http_request_uri),
+                Arc::new(http_host),
+                Arc::new(http_request_method),
+                Arc::new(http_user_agent),
+                Arc::new(http_file_data),
+                Arc::new(nttp_priv_reqcode),
+                Arc::new(pcap_file),
+                ]
+        ).unwrap();
+
+        return batch;
+
+    }
+
+
+    pub fn flush(&mut self) {
+        self.write_batch();
+    }
+
+    fn write_batch(&mut self) {
+        let batch = self.record_batch();
+        self.writer.write(&batch).unwrap();
+        self.packets = Vec::new();
+
+        if self.verbose {
+            eprint!("\rPackets: {} Cache misses: {}", 
+                self.packet_count.to_formatted_string(&Locale::en), 
+                self.cache_misses.to_formatted_string(&Locale::en));
+        }
+    }
+ 
+    pub fn close_parquet(&mut self) {
+        self.flush();
+        self.writer.flush();
+    }
+
+}
+
+
